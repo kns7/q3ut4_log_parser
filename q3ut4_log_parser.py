e5aadaba14539c494b6e41ea5f30838c87565a89
Nicolas Kapfer <nico@kns7.org>
2020-04-05 18:31:27 +0200

    Corrected link to Chart.js (CDNJS)

--- q3ut4_log_parser.py
+++ q3ut4_log_parser.py
@@ -733,7 +733,7 @@ def main():
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
         crossorigin="anonymous"></script>
-	<script src="https://cdnjs.com/libraries/Chart.js"></script>    
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>    
     <script src="script.js"></script>		
 </head>
 
